b3240252793259659284831f1fe281bddabbd428
nidamrtdlh <nidamrtdlh16@gmail.com>
2021-09-24 16:36:07 +0700

    menambahkan contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Nida Murtadoilah
+            Saya tinggal di daerah Gunungtanjung Kab Tasikmalaya
+            olah raga yang saya sukai  adalah
+            basket No HP 085721724977";
+    }
+}
